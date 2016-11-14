cdb11b75cbb6fceb03de60e33d39c405cc894b46
Sebastian Stark <sstark@mailbox.org>
2016-11-14 15:42:09 +0100

    split store code from main program logic

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	//"html"
-	"crypto/sha256"
 	"io"
 	"io/ioutil"
 	"log"
@@ -21,67 +19,6 @@ const (
 	uGet       = "/g"
 	maxData    = 1048576 // 1MB
 )
-
-// In-memory representation of a secret.
-type StoreEntry struct {
-	Secret    string    `json:"secret"`
-	MaxClicks int       `json:"max_clicks"`
-	Clicks    int       `json:"clicks"`
-	DateAdded time.Time `json:"date_added"`
-}
-
-// Secret augmented with computed fields.
-type StoreEntryInfo struct {
-	StoreEntry
-	Id        string `json:"id"`
-	PathQuery string `json:"path_query"`
-	Url       string `json:"url"`
-}
-
-type secretStore map[string]StoreEntry
-
-// hashStruct returns a hash from an arbitrary structure, usable in a URL.
-func hashStruct(data interface{}) (hash string) {
-	hashBytes := sha256.Sum256([]byte(fmt.Sprintf("%#v", data)))
-	hash = base64.URLEncoding.EncodeToString(hashBytes[:])
-	return
-}
-
-// NewEntry adds a new secret to the store.
-func (st secretStore) NewEntry(e StoreEntry) string {
-	e.DateAdded = time.Now()
-	id := hashStruct(e)
-	st[id] = e
-	return id
-}
-
-// GetEntry retrieves a secret from the store.
-func (st secretStore) GetEntry(id string) (se StoreEntry, ok bool) {
-	se, ok = st[id]
-	return
-}
-
-// GetEntryInfo wraps GetEntry and adds some computed fields.
-func (st secretStore) GetEntryInfo(id string) (si StoreEntryInfo, ok bool) {
-	entry, ok := st.GetEntry(id)
-	pathQuery := uGet + "?" + id
-	url := schemeHost + listen + pathQuery
-	return StoreEntryInfo{entry, id, pathQuery, url}, ok
-}
-
-// Click increases the click counter for an entry.
-func (st secretStore) Click(id string) {
-	entry, ok := st.GetEntry(id)
-	if ok {
-		if entry.Clicks < entry.MaxClicks-1 {
-			entry.Clicks += 1
-			st[id] = entry
-		} else {
-			delete(st, id)
-		}
-	}
-	return
-}
 
 func main() {
 	store := make(secretStore)
