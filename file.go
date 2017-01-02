617c10dbc29eb9f4013044d536ea5cc99801882c
Sebastian Stark <sstark@mailbox.org>
2017-01-02 12:02:51 +0100

    split tryReadFile() in two parts
      - one part, tryFile(), to just find the file
      - another part to read it
      - interface stays the same

--- file.go
+++ file.go
@@ -7,10 +7,22 @@ import (
 	"path"
 )
 
-// tryReadFile takes a _filename_ as an argument and tries to read that file
-// from cwd and then configDir. If it doesn't find the file in any of them it
-// will return an empty byte slice.
+// tryFile takes a _filename_ and uses tryFile() to find the file and
+// eventually return its contents. If the files was not found or is unreadable
+// returns an empty byte slice.
 func tryReadFile(fn string) []byte {
+	pn := tryFile(fn)
+	contents, err := ioutil.ReadFile(pn)
+	if err == nil {
+		return contents
+	}
+	return []byte{}
+}
+
+// tryFile takes a _filename_ as an argument and tries several directories to
+// find this file. In the case of success it returns the full path name,
+// otherwise it returns the empty string.
+func tryFile(fn string) string {
 	var dirs []string
 	cwd, err := os.Getwd()
 	if err == nil {
@@ -20,12 +32,14 @@ func tryReadFile(fn string) []byte {
 	}
 	dirs = append(dirs, configDir)
 	for _, dir := range dirs {
-		contents, err := ioutil.ReadFile(path.Join(dir, fn))
+		pn := path.Join(dir, fn)
+		f, err := os.Open(pn)
 		if err == nil {
 			log.Printf("found %s in %s\n", fn, dir)
-			return contents
+			f.Close()
+			return pn
 		}
 	}
 	log.Println("could not find", fn)
-	return []byte{}
+	return ""
 }
